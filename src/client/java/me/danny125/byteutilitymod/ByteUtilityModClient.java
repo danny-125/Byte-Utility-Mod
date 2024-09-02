9add811fd62ae6b42daa92aa63de291343ba94c7
danny-125 <90357372+danny-125@users.noreply.github.com>
2024-09-02 16:20:38 -0600

    Initial commit
    sigma

---
+++ src/client/java/me/danny125/byteutilitymod/ByteUtilityModClient.java
@@ -0,0 +1,10 @@
+package me.danny125.byteutilitymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ByteUtilityModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
